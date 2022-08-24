77a006432319d7d78d9b4e409f9ce16549e4113f
tsukimiya-wk <wohushiqi@outlook.com>
2022-08-24 21:38:01 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+/**
+ * Implement the composable function
+ * Make sure the function works correctly
+ */
+function useLocalStorage(key: string, initialValue: any) {
+  const value = ref(localStorage.getItem(key) ?? initialValue);
+
+  watch(value, (newValue) => {
+    localStorage.setItem(key, newValue);
+  });
+
+  return value;
+}
+
+const counter = useLocalStorage('counter', 0);
+
+// We can get localStorage by triggering the getter:
+console.log(counter.value);
+
+// And we can also set localStorage by triggering the setter:
+
+const update = () => counter.value++;
+</script>
+
+<template>
+  <p>Counter: {{ counter }}</p>
+  <button @click="update">Update</button>
+</template>
---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-s8wgts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-s8wgts)
